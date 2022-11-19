d19bde90839d701e13220fb7172c5aa8ece23ea5
Adam Gaboury <adam.gaboury@hotmail.com>
2022-11-18 23:38:21 -0500

    grid image for projects code added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/index';
 import Footer from './components/Footer/index';
+import GridImages from './components/Cards'; 
 
 function App() {
   return (
     <div>
       <Header></Header>
+        <GridImages/>
       <Footer></Footer>
     </div>
   );
